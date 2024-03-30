2ae1531e834f3585b35d39f87f6a8c3d058e9386
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 22:42:54 -1200

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,21 @@ Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Returns the nunmber of each object type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
