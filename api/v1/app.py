21fb2046569e4be1cfd00f624111c05a7345baff
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 22:49:08 -1200

    404 not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,11 @@
 """
 Flask route that returns json status response
 """
-from flask import Flask
+from flask import Flask , jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,7 +15,15 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     storage.close()
 
+@app.errorhandler(NotFound)
+def handle_not_found(error):
+    """Handler for 404 Not Found errors"""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host='0.0.0.0', port=5000)
     app.run(host=host, port=port, threaded=True)
