c7e13aa4341408a7f8c25aebf468e90ee662ce74
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 22:09:19 -1200

    status response

---
+++ api/v1/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Flask route that returns json status response
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def api_status():
+    """Function that returns json status response"""
+    return jsonify({"status": "OK"})
