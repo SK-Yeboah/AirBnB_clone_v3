6107126173c88ba8172da4ca9c0080959146a955
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 22:38:37 -1200

    status response_1

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,19 @@
 """
 Flask route that returns json status response
 """
+from flask import Flask
+from models import storage
 from api.v1.views import app_views
-from flask import jsonify
+import os
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def api_status():
-    """Function that returns json status response"""
-    return jsonify({"status": "OK"})
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
